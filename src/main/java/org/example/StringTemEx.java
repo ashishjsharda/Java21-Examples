42a4096c74aa731591418fa0d071b24a6fa16d59
Ashish Sharda <ashishjsharda@gmail.com>
2023-11-13 23:18:37 -0500

    Fix empty files

--- src/main/java/org/example/StringTemEx.java
+++ src/main/java/org/example/StringTemEx.java
@@ -1,2 +1,14 @@
-package org.example;public class StringTemEx {
+package org.example;
+
+
+
+public class StringTemEx {
+    public static void main(String[] args) {
+//        StringTemplate template=StringTemplate.of("Hello ,${name} You are ${age} years old");
+//        Map<String,Object> values =new HashMap<>();
+//        values.put("name","John");
+//        values.put("age",20);
+//        STR.process(template);
+
+    }
 }
